19e6e27ab96b49d135ee59e7258c84034925b2b7
DangLun <danglun337747@gmail.com>
2024-07-30 13:35:26 +0700

    update 3007

---
+++ FlowerShop/CustomValidations/IsValidTopic.cs
@@ -0,0 +1,24 @@
+﻿using FlowerShop.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace FlowerShop.CustomValidations
+{
+	public class IsValidTopic : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string topicName = value as string;
+                FlowerShopDbContext db = new FlowerShopDbContext();
+                var e = db.Topics.FirstOrDefault(x => x.TopicName == topicName);
+                if (e != null)
+                {
+                    return new ValidationResult("Chủ đề đã tồn tại");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
